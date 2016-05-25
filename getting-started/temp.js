a49d3deb32dd4b82ed0e5628db3f5f775ac35754
Joseph Stahl <joseph.e.stahl@vanderbilt.edu>
2016-05-25 16:13:43 -0500

    feedback

--- getting-started/temp.js
+++ getting-started/temp.js
@@ -1,12 +1,45 @@
-var cy = cytoscape({
-  container: document.getElementById('cy'),
-  elements: [
-    {data: {id: 'a'}},
-    {data: {id: 'b'}},
-    {data: {
+elements: [
+  // nodes
+  { data: { id: 'a' } },
+  { data: { id: 'b' } },
+  { data: { id: 'c' } },
+  { data: { id: 'd' } },
+  { data: { id: 'e' } },
+  { data: { id: 'f' } },
+  // edges
+  {
+    data: {
       id: 'ab',
       source: 'a',
       target: 'b'
     }
-    }]
-});
+  },
+  {
+    data: {
+      id: 'cd',
+      source: 'c',
+      target: 'd'
+    }
+  },
+  {
+    data: {
+      id: 'ef',
+      source: 'e',
+      target: 'f'
+    }
+  },
+  {
+    data: {
+      id: 'ac',
+      source: 'a',
+      target: 'd'
+    }
+  },
+  {
+    data: {
+      id: 'be',
+      source: 'b',
+      target: 'e'
+    }
+  }
+]
