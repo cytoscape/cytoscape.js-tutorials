44595b71a7aad85298c6d46a8fb7097d855f9cdb
Joseph Stahl <joseph.e.stahl@vanderbilt.edu>
2016-06-08 22:42:59 -0500

    start collecting twitter user data

--- twitter_api/main.js
+++ twitter_api/main.js
@@ -1,11 +1,20 @@
 var Twit = require('twit');
+var fs = require('fs');
 
 var T = new Twit({
   consumer_key: 'VIP8NsuVgTdAV2EViWQt3PKPH',
   consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
-  access_token: '371074472-cLqYUGbYB5q9hx9cl5KhiaeW7nzo2MypXbMJ7s53',
-  access_token_secret: process.env.TWITTER_ACCESS_SECRET,
+  app_only_auth: true,
   timeout_ms: 60 * 1000
 });
 
+var outputFile = 'twitter_api/followers.txt';
 
+function getFollowerPromise(screenName) {
+  return T.get('followers/ids', { screen_name: screenName });
+}
+
+getFollowerPromise('cytoscape')
+  .then(function(result) {
+    fs.writeFile(outputFile, result.data.ids);
+  });
