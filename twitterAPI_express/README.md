99039dea7e6251a41fd03aa23749cf5840a84840
Joseph Stahl <joseph.e.stahl@vanderbilt.edu>
2016-06-13 00:21:06 -0500

    Use Express for a Twitter API

---
+++ twitterAPI_express/README.md
@@ -0,0 +1,46 @@
+# Twitter API for Cytoscape.js Tutorial
+
+API paths:
+
+- POST /twitter/followers (from Postman)
+
+```javascript
+var data = "username=josephst18";
+var xhr = new XMLHttpRequest();
+xhr.withCredentials = true;
+
+xhr.addEventListener("readystatechange", function () {
+  if (this.readyState === 4) {
+    console.log(this.responseText);
+  }
+});
+
+xhr.open("POST", "http://localhost:3000/twitter/user");
+xhr.setRequestHeader("cache-control", "no-cache");
+xhr.setRequestHeader("postman-token", "092e4080-5e77-20c0-952d-ec0a36cef59c");
+xhr.setRequestHeader("content-type", "application/x-www-form-urlencoded");
+
+xhr.send(data);
+```
+
+- POST /twitter/followers
+
+```javascript
+var data = "username=josephst18";
+
+var xhr = new XMLHttpRequest();
+xhr.withCredentials = true;
+
+xhr.addEventListener("readystatechange", function () {
+  if (this.readyState === 4) {
+    console.log(this.responseText);
+  }
+});
+
+xhr.open("POST", "http://localhost:3000/twitter/followers");
+xhr.setRequestHeader("cache-control", "no-cache");
+xhr.setRequestHeader("postman-token", "1e7c5a24-bbe7-d3cd-6251-16b3a8dad350");
+xhr.setRequestHeader("content-type", "application/x-www-form-urlencoded");
+
+xhr.send(data);
+```
---
+++ twitterAPI_express/routes/twitter.js
@@ -0,0 +1,46 @@
+var express = require('express');
+var router = express.Router();
+var Twit = require('twit');
+
+var T = new Twit({
+  consumer_key: 'VIP8NsuVgTdAV2EViWQt3PKPH',
+  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+  app_only_auth: true,
+  timeout_ms: 60 * 1000
+});
+
+/* POST followers. */
+router.post('/followers', function(req, res, next) {
+  var followers = [];
+  var username = req.body.username;
+  T.get('followers/list',
+    { screen_name: username, count: 200, skip_status: true },
+    function collectData(err, data) {
+      if (err) {
+        throw err;
+      }
+
+      followers = followers.concat(data.users);
+
+      if (data.next_cursor === 0) {
+        res.send(JSON.stringify(followers, null, 4));
+      } else {
+        T.get('followers/list',
+          { screen_name: username, count: 200, skip_status: true, cursor: data.next_cursor },
+          collectData);
+      }
+    });
+});
+
+/* POST user information. */
+router.post('/user', function(req, res, next) {
+  var username = req.body.username;
+  T.get('users/show', { screen_name: username }, function(err, data) {
+    if (err) {
+      throw err;
+    }
+    res.send(JSON.stringify(data, null, 4));
+  });
+});
+
+module.exports = router;
