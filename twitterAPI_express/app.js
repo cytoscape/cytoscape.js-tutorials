9e87ef5d5ac1c859ef009eb1978f64b20dcbfb39
Joseph Stahl <joseph.e.stahl@vanderbilt.edu>
2016-06-13 00:37:25 -0500

    clean up test route

--- twitterAPI_express/app.js
+++ twitterAPI_express/app.js
@@ -7,7 +7,6 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var twitter = require('./routes/twitter');
-var test = require('./routes/test');
 
 var app = express();
 
@@ -25,7 +24,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/twitter', twitter);
-app.use('/test', test);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
